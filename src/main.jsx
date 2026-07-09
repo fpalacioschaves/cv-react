c144ef171fce5f6bade49c42237026fca75bfb3f
Francisco Palacios Chaves <fpalacioschaves@gmail.com>
2026-07-09 16:19:10 +0200

    Import print cleanup stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './responsive.css'
+import './print.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
